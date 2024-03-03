d92e923411fadf22899d9723808639d6b78e90c6
H-S <taip21401@gmail.com>
2024-03-04 01:19:00 +0200

    finish splash and home screen

---
+++ lib/core/config/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:untitled2/main.dart';
+
+class   Constant{
+  static  var mediaQuery=MediaQuery.of(navigatorKey.currentState!.context).size;
+  static var theme=Theme.of(navigatorKey.currentState!.context);
+}
---
+++ lib/core/config/page_route_name.dart
@@ -0,0 +1,6 @@
+ class PagesRouteName
+ {
+static const String initial="/";
+static const String home_view="home_view";
+static const String setting_view="setting_view";
+ }
